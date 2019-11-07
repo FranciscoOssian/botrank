6d356ac3470689d5198c5c387bd92478951512aa
Francisco Ossian <42554801+FranciscoOssianFOLN@users.noreply.github.com>
2019-11-07 12:57:05 -0300

    Update bot.js

--- bot.js
+++ bot.js
@@ -1,17 +1,9 @@
 
-
-
-// Import the discord.js module
 const Discord = require('discord.js');
 const Config = require('./Config.js')
 
-// Create an instance of a Discord client
 const client = new Discord.Client();
 
-/**
- * The ready event is vital, it means that only _after_ this will your bot start reacting to information
- * received from Discord
- */
 client.on('ready', () => {
   console.log('I am ready!');
 });
@@ -40,5 +32,5 @@ client.on('message', message => {
   
 });
 
-// Log our bot in using the token from https://discordapp.com/developers/applications/me
+
 client.login(process.env.bot_token);
