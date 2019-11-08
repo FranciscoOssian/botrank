73302230bbbc0c00b8d7cb94f7abe9a1b3f8ccc6
Francisco Ossian <42554801+FranciscoOssianFOLN@users.noreply.github.com>
2019-11-08 14:21:20 -0300

    create comands.js

---
+++ Comands.js
@@ -0,0 +1,10 @@
+const Say = (message) => {
+    message.channel.send(message.content.slice(4))
+    message.delete().catch(O_o=>{}); 
+}
+
+module.exports.Say = Say
+
+const NewHome = (message) => {
+
+}
