55b4254b47aaedf66289bb5a1acb639f1db6120b
Francisco Ossian <42554801+FranciscoOssianFOLN@users.noreply.github.com>
2019-12-13 10:27:56 -0300

    Update Comands.js

--- Comands.js
+++ Comands.js
@@ -6,33 +6,25 @@ const Say = (message) => {
 
 const channelView = (message, view) => {
     doIt = (message, value) => {
-        message.channel.send('entrou no doit');
         if(value === true){
-            message.channel.send('entrou no if do doit');
             message.channel.overwritePermissions(message.channel.guild.defaultRole, { READ_MESSAGES: true });
         }
         else{
-            message.channel.send('entrou no else do doit');
             message.channel.overwritePermissions(message.channel.guild.defaultRole, { READ_MESSAGES: false });
         }
         return 0;
     }
     
     for (pessoa of message.channel.permissionOverwrites){
-        message.channel.send('entrou no for');
         pessoa = pessoa[1];
         if(message.author.id === pessoa.id && pessoa.allow === 805829713){
-            message.channel.send('entrou no if do for');
             if(view === false){
-                message.channel.send('entrou no if do if do for');
                 doIt(message, false);
             }else{
-                message.channel.send('entrou no else do if do if do for');
                 doIt(message, true);
             }
             return 0;
         }else{
-            message.channel.send('ahhhhh');
             message.channel.send('n~ao adm ou permissoes adulteradas');
         }
     }
