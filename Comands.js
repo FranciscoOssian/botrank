5087e1ed2a1ba7aa3f278031e738ad19388b6553
Francisco Ossian <42554801+FranciscoOssianFOLN@users.noreply.github.com>
2019-12-13 01:16:31 -0300

    update comands.js update channelView

--- Comands.js
+++ Comands.js
@@ -1,15 +1,36 @@
+const Config = require('./Config.js');
+
 const Say = (message) => {
     message.channel.send(message.content.slice(4))
     message.delete().catch(O_o=>{}); 
 }
 
-module.exports.Say = Say
-
-const NewHome = (message) => {
-
-}
-
 const SetPrefix = (prefix) => {
     Config.SetPrefix(prefix);
 }
-module.exports.SetPrefix = SetPrefix
+
+function doIt(message, value){
+    message.channel.overwritePermissions(message.channel.guild.defaultRole, { VIEW_CHANNEL: value });
+    message.channel.send('sala ocultada de everyone, por favor nao mude suas permisoes');
+}
+const channelView = (message, view) => {
+    for (pessoa of message.channel.permissionOverwrites){
+        pessoa = pessoa[1];
+        if(message.author.id === pessoa.id && pessoa.allow === 805829713){
+            if(view === false){
+                doIt(message, false);
+            }else{
+                doIt(message, true);
+            }
+            return 0;
+        }else{
+            message.channel.send('n~ao adm ou permissoes adulteradas');
+        }
+    }
+}
+
+
+
+module.exports.SetPrefix = SetPrefix;
+module.exports.Say = Say;
+module.exports.channelView = channelView;
