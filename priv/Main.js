aed438c85f414a7e7bc0252185a21a275e50ab34
Francisco Ossian <42554801+FranciscoOssianFOLN@users.noreply.github.com>
2019-12-15 20:42:53 -0300

    update main.js help comand

--- priv/Main.js
+++ priv/Main.js
@@ -1,14 +1,16 @@
 const SayModule = require('./Comands/Say.js');
 const channelViewModule = require('./Comands/channelView.js');
 const banMemberModule = require('./Comands/banMember.js');
+const helpModule = require('./Comands/help/help.js');
 
 var Say = SayModule.Say;
 var banMember = banMemberModule.banMember;
 var channelView = channelViewModule.channelView;
+var help = helpModule.help;
 
 
 
-
+module.exports.help = help;
 module.exports.Say = Say;
 module.exports.banMember = banMember;
 module.exports.channelView = channelView;
