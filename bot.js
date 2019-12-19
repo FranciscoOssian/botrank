d2ca573c02a283bb4a9e54d4fd54b4afc1903e9a
Francisco Ossian <42554801+FranciscoOssianFOLN@users.noreply.github.com>
2019-12-19 01:34:09 -0300

    Update bot.js

--- bot.js
+++ bot.js
@@ -59,24 +59,8 @@ client.on('message', message =>{
             break;
         }
         case 'wasted':{
-            var images = [message.member.avatarURL, 'sticker_2121.png'];
-            var jimps = [];
-
-
-            for(image of images){
-                jimps.push(Jimp.read(image));
-            }
-
-            Promise.all(jimps).then(function(data){
-                return Promise.all(jimps);}).then(function(data){
-
-                    data[0].greyscale();
-                    data[0].composite(data[1], 0, 0);
-                    data[0].write('./testef.png');
-
-                    message.channel.send('image:', { files:["./testef.png"] } );
-
-                }).catch(function(err){ console.log(err) });
+            var args = message.content.slice(Config.GetConfig().prefix.length).trim().split(/ +/g);
+            Comands.wasted(message, args);
             break;
         }
         default:{
